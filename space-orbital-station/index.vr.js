0d12762378826650cc0a3140103e4d3cf66ef7a8
Andrelbarreto <andrebarreto77@gmail.com>
2020-03-28 08:00:32 -0500

    New static assets for components

--- space-orbital-station/index.vr.js
+++ space-orbital-station/index.vr.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-import React from 'react';
-=======
+
 import React, { Component } from 'react';
->>>>>>> 6da4fe25a69a801f17f251bfeac38d0b2b9307f2
 import {
   AppRegistry,
   asset,
@@ -11,11 +8,9 @@ import {
   View,
 } from 'react-vr';
 
-<<<<<<< HEAD
-=======
-import Control from './components/control-button'
+import Control from './components/control-button';
+import Control_tip1 from '/components/Control_tip1';
 
->>>>>>> 6da4fe25a69a801f17f251bfeac38d0b2b9307f2
 export default class space_orbital_station extends React.Component {
   render() {
     return (
@@ -24,29 +19,19 @@ export default class space_orbital_station extends React.Component {
         <Text
           style={{
             backgroundColor: '#000080',
-<<<<<<< HEAD
             fontSize: 0.5,
             fontWeight: '400',
             layoutOrigin: [0.5, 0.5],
-=======
-            fontSize: 0.3,
-            fontWeight: '300',
-            layoutOrigin: [+0.5, 0.5],
->>>>>>> 6da4fe25a69a801f17f251bfeac38d0b2b9307f2
             paddingLeft: 0.2,
             paddingRight: 0.2,
             textAlign: 'center',
             textAlignVertical: 'center',
             transform: [{translate: [0, 0, -3]}],
           }}>
-<<<<<<< HEAD
-          Space Orbital Scape
-        </Text>
-=======
           S. O. S.
         </Text>
         <Control />
->>>>>>> 6da4fe25a69a801f17f251bfeac38d0b2b9307f2
+        <Control_tip1 />
       </View>
     );
   }
