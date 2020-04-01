1ca8298395492729abd8e25133a4397917fc1fbc
Shannon Ervin <sjervin07@gmail.com>
2020-04-01 17:10:47 -0500

    Server running

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-const express = require('express')
+const express = require("express");
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const session = require('express-session')
 const dbConnection = require('./database') 
 const MongoStore = require('connect-mongo')(session)
 const passport = require('./passport');
-const app = express()
-const PORT = 8080
-// Route requires
-const user = require('./routes/user')
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+const user = require('./routes/api/users')
+require('./config/passport');
+require('./database/models/users');
+require("dotenv").config();
+
 
 // MIDDLEWARE
 app.use(morgan('dev'))
@@ -19,17 +24,29 @@ app.use(
 )
 app.use(bodyParser.json())
 
-// Sessions
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Add routes, both API and view
+app.use(routes);
+
+
 app.use(
 	session({
-		secret: 'secret-spaceman', //pick a random string to make the hash that is generated secure
+		secret: 'secret-astroman', //pick a random string to make the hash that is generated secure
 		store: new MongoStore({ mongooseConnection: dbConnection }),
 		resave: false, //required
 		saveUninitialized: false //required
 	})
 )
 
-// Passport
+
+  // Passport
 app.use(passport.initialize())
 app.use(passport.session()) // calls the deserializeUser
 
@@ -37,7 +54,7 @@ app.use(passport.session()) // calls the deserializeUser
 // Routes
 app.use('/user', user)
 
-// Starting Server 
-app.listen(PORT, () => {
-	console.log(`App listening on PORT: ${PORT}`)
-})
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
