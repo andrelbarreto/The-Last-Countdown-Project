fa7f9e90a52c04684e9bffe0864a3feab8bd3389
Shannon Ervin <sjervin07@gmail.com>
2020-04-01 15:23:24 -0500

    reinitializing server.js

--- server.js
+++ server.js
@@ -1,37 +1,43 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const routes = require("./routes");
-const app = express();
-const PORT = process.env.PORT || 3001;
-require('./config/passport');
-require('./models/Users');
-require("dotenv").config();
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const session = require('express-session')
+const dbConnection = require('./database') 
+const MongoStore = require('connect-mongo')(session)
+const passport = require('./passport');
+const app = express()
+const PORT = 8080
+// Route requires
+const user = require('./routes/user')
+
+// MIDDLEWARE
+app.use(morgan('dev'))
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+)
+app.use(bodyParser.json())
+
+// Sessions
+app.use(
+	session({
+		secret: 'secret-spaceman', //pick a random string to make the hash that is generated secure
+		store: new MongoStore({ mongooseConnection: dbConnection }),
+		resave: false, //required
+		saveUninitialized: false //required
+	})
+)
+
+// Passport
+app.use(passport.initialize())
+app.use(passport.session()) // calls the deserializeUser
 
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-// Add routes, both API and view
-app.use(routes);
+// Routes
+app.use('/user', user)
 
-// Connect to the Mongo DB
- mongoose.connect(
-    process.env.MONGODB_URL || "mongodb://localhost/VR_Data",
-  {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  },
-  
-  () =>
-    console.log("connected to DB!")
-  );
-
-// Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-});
+// Starting Server 
+app.listen(PORT, () => {
+	console.log(`App listening on PORT: ${PORT}`)
+})
